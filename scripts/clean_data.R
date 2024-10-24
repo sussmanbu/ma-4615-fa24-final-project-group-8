0ffcacee2eb409b6bdac18c67378b3b89c920c37
Maika Takemoto <maikat@bu.edu>
2024-10-24 18:53:37 -0400

    Added clean_data.R file and cleaned data. Also added to data.qmd file to load in both original and cleaned data sets.

---
+++ scripts/clean_data.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(here)
+
+our_data <- read_csv(here::here("dataset", "ELSIPreliminaryDataSet.csv"), skip = 5, col_names = TRUE)
+
+# Clean the data set
+# Remove rows with "†", "-", "‡", or negative values
+our_data_clean <- our_data |>
+  filter_all(all_vars(!str_detect(., "†|\\–|‡"))) |>
+  filter_all(all_vars(as.numeric(.) >= 0 | is.na(as.numeric(.))))
+
+# Save the cleaned data as an .rds file
+write_rds(our_data_clean, file = here::here("dataset", "our_data_clean.rds"))
