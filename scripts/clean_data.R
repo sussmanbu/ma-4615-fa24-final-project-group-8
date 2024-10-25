3e4040ee9b55fc72883ad26c4a8eebac73476421
Richard Pilleul <richardp@bu.edu>
2024-10-25 07:50:53 -0400

    Further cleaning. Adjusted State names

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -7,7 +7,9 @@ our_data <- read_csv(here::here("dataset", "ELSIPreliminaryDataSet.csv"), skip =
 # Remove rows with "†", "-", "‡", or negative values
 our_data_clean <- our_data |>
   filter_all(all_vars(!str_detect(., "†|\\–|‡"))) |>
-  filter_all(all_vars(as.numeric(.) >= 0 | is.na(as.numeric(.))))
+  filter_all(all_vars(as.numeric(.) >= 0 | is.na(as.numeric(.)))) |>
+  mutate(`State Name [Public School] Latest available year` = str_to_title(`State Name [Public School] Latest available year`))
 
 # Save the cleaned data as an .rds file
 write_rds(our_data_clean, file = here::here("dataset", "our_data_clean.rds"))
+
