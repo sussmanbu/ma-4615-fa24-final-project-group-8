3dec2cbf6997f3cf0cee22da456c3ac647a7b452
Maika Takemoto <maikat@bu.edu>
2024-11-13 16:59:53 -0500

    Added new dataset and cleaned it

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -13,3 +13,16 @@ our_data_clean <- our_data |>
 # Save the cleaned data as an .rds file
 write_rds(our_data_clean, file = here::here("dataset", "our_data_clean.rds"))
 
+our_data <- read_csv(here::here("dataset", "ELSIPreliminaryDataSet.csv"), skip = 5, col_names = TRUE)
+
+# Clean the new data set - for Jakob's use
+new_data <- read_csv(here::here("dataset", "ELSIDataSet2.csv"), skip = 5, col_names = TRUE)
+
+# Remove rows with "†", "-", "‡", or negative values
+new_data_clean <- new_data |>
+  filter_all(all_vars(!str_detect(., "†|\\–|‡"))) |>
+  filter_all(all_vars(as.numeric(.) >= 0 | is.na(as.numeric(.)))) |>
+  mutate(`State Name [Public School] Latest available year` = str_to_title(`State Name [Public School] Latest available year`))
+
+# Save the cleaned data as an .rds file
+write_rds(new_data_clean, file = here::here("dataset", "our_data_clean.rds"))
